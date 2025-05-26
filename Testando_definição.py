d89ab5e8c5f22bc1520316bc715d42f8946cc2cf
Marco-Cesar <viegascesar350@gmail.com>
2025-05-26 15:25:41 -0400

    Add files via upload

--- Testando_definição.py
+++ Testando_definição.py
@@ -1,17 +1,38 @@
-compras = ["pão","cafe","leite"]
-print(compras)
+#compras = ["pão","cafe","leite"]
+#print(compras)
 
 #pode ser removido pelo nome do prduto ou pelo indice
 #compras.remove("cafe")
-compras.remove(compras[0])
-print(compras)
+#compras.remove(compras[0])
+#print(compras)
 
 #append acrescenta um item no final da lista, só pode adicionar um por vez
-compras.append("açucar")
-print(compras)
+#compras.append("açucar")
+#print(compras)
 #é preciso criar uma lista nova para receeber os elementos ordenados da lista antiga
-compras_ordenada = sorted(compras)
-print(compras_ordenada)
+#compras_ordenada = sorted(compras)
+#print(compras_ordenada)
 #o nome panela é um identificado dos itens dentro da lista, esses itens podem receber qualquer tipo de nome
-for panela in compras:
-    print("-", panela)
+#for panela in compras:
+#    print("-", panela)
+
+#listas []
+#dicionários= {chave:valor}
+#tuplas= ()
+#conjuntos = set()
+#uma lista
+#   teste=[1, 2, 3, 5]
+#   print(teste)
+#   teste.append(4)
+#   print(teste)
+#uma tupla
+#   teste=(1, 2, 3, 5)
+#   print(teste)
+#   teste.remove(1)
+#   print(teste)
+#um conjuto
+#teste={1, 2, 3, 5,5,6,6}
+#print(teste)
+#teste.remove(1)
+#print(teste)
+#teste.append("1")
